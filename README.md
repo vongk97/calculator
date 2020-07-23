1904917f65187b5861c1866699638355a5edc523
Kevin Vong <vongk97@gmail.com>
2020-07-23 12:30:38 -0700

    Main endpoints complete

--- README.md
+++ README.md
@@ -4,5 +4,24 @@
     -All digits
     -Operators
     -Clear (AC)
-    -Backspace (CE)
+    -EXTRA CREDIT: Backspace (CE)
     -Equals (=)
+
+7-22-20
+  -Changes:
+    -Can perform operations such as 1 + 5 * 3 - 4 without having to press = between each operation
+    -Prevents errors upon pressing an operator multiple times in succession
+    -Allows Pressing = before entering all of the numbers or an operator
+
+  -Errors
+    -Handle repeated decimal entries
+
+7-23-20
+  -Main Endpoints complete!!!
+  
+  -Changes:
+    -EXTRA CREDIT: Let users enter decimals. Only allow one decimal for each operand
+
+  -To Do:
+    -EXTRA CREDIT: Make it look nice!
+    -EXTRA CREDIT: Add keyboard support
--- main.js
+++ main.js
@@ -46,48 +46,82 @@ function nine() {
 }
 
 function dot() {
+  if(num.includes(".")) { return ;}
   num += ".";
   document.getElementById("display").value = num;
 }
 
 function ac() {
-  num = "";
-  firstOperand = "";
-  secondOperand = "";
   document.getElementById("history").value = "";
   document.getElementById("display").value = "";
+  num = "";
+  firstOperand = null;
+  secondOperand = null;
+  output = null;
+  operator = "";
 }
 
 function ce() {
+  if(num == "") { return; }
   num = num.substring(0, num.length-1);
   document.getElementById("display").value = num;
 }
 
 function divide() {
-  firstOperand = parseFloat(num);
-  document.getElementById("history").value = firstOperand + " / ";
-  num = "";
-  operator = "/";
+  if(operator=="") {
+    firstOperand = parseFloat(num);
+    document.getElementById("history").value = firstOperand + " / ";
+    num = "";
+    operator = "/";
+  }
+  else {
+    operate();
+    operator = "/";
+    num = "";
+  }
 }
 function multiply() {
-  firstOperand = parseFloat(num);
-  document.getElementById("history").value = firstOperand + " * ";
-  num = "";
-  operator = "*";
+  if(operator=="") {
+    firstOperand = parseFloat(num);
+    document.getElementById("history").value = firstOperand + " * ";
+    num = "";
+    operator = "*";
+  }
+  else {
+    operate();
+    operator = "*";
+    num = "";
+  }
 }
 function addition() {
-  firstOperand = parseFloat(num);
-  document.getElementById("history").value = firstOperand + " + ";
-  num = "";
-  operator = "+";
-}
-function subtraction() {
-  firstOperand = parseFloat(num);
-  document.getElementById("history").value = firstOperand + " - ";
-  num = "";
-  operator = "-";
+  if(operator=="") {
+    firstOperand = parseFloat(num);
+    document.getElementById("history").value = firstOperand + " + ";
+    num = "";
+    operator = "+";
+  }
+  else {
+    operate();
+    operator = "+";
+    num = "";
+  }
 }
 
+function subtraction() {
+  if(operator=="") {
+    firstOperand = parseFloat(num);
+    document.getElementById("history").value = firstOperand + " - ";
+    num = "";
+    operator = "-";
+  }
+  else {
+    operate();
+    operator = "-";
+    num = "";
+  }
+}
+
+/*
 function equals() {
   secondOperand = parseFloat(num);
   if (operator == "/") {
@@ -121,4 +155,51 @@ function equals() {
   }
 
   secondOperand = null;
+}
+*/
+
+/*
+1. Enter digits
+2. enter an operator. operate function is called. digits are stored in firstOperand. operator symbol is stored. secondOperand is NULL, so end function
+3. Enter digits
+4. Enter an operator or equal symbol. operate function called. firstOperand is NOT null, so store digits in secondOperand. perform operation based on previous operator.
+    5. Store output in firstOperand. change secondOperand to null. change operator to the most recent one.
+
+*/
+function operate() {
+  // if num == "", then an operator symbol has been pressed twice
+  if(num==""){ return; }
+  secondOperand = parseFloat(num);
+
+  if (operator == "*") {
+    output = firstOperand * secondOperand;
+    document.getElementById("history").value = firstOperand + " * " + secondOperand;
+    document.getElementById("display").value = output;
+    firstOperand = output;
+    num = "";
+  }
+  if (operator == "/") {
+    if(secondOperand == 0) { document.getElementById("display").value = "CANNOT DIVIDE BY ZERO!!!"; }
+    else {
+      output = firstOperand / secondOperand;
+      document.getElementById("history").value = firstOperand + " / " + secondOperand;
+      document.getElementById("display").value = output;
+      firstOperand = output;
+      num = "";
+    }
+  }
+  if (operator == "+") {
+    output = firstOperand + secondOperand;
+    document.getElementById("history").value = firstOperand + " + " + secondOperand;
+    document.getElementById("display").value = output;
+    firstOperand = output;
+    num = "";
+  }
+  if (operator == "-") {
+    output = firstOperand - secondOperand;
+    document.getElementById("history").value = firstOperand + " - " + secondOperand;
+    document.getElementById("display").value = output;
+    firstOperand = output;
+    num = "";
+  }
 }
